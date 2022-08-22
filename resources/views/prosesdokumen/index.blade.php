81a2553ab0da3640bf4bc2b5c8476da5e1da2aac
IqbalCaraka <iqbalcaraka@gmail.com>
2022-08-22 11:17:29 +0700

    membuat simple qrcode

--- resources/views/prosesdokumen/index.blade.php
+++ resources/views/prosesdokumen/index.blade.php
@@ -31,7 +31,7 @@
 </div>
 
 
-<!--Modal show aksi-->
+<!--Modal show tanda tangan-->
 <div class="modal fade" id="modalAksi" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
         <div class="modal-content">
@@ -53,7 +53,9 @@
                                                 <div class="check_kasub_umum mb-3">
                                                 </div> 
                                                 <div>
-                                                    <a href="#" class="btn btn-primary btn-sm">Tanda Tangan</a>
+                                                    <a href="#">
+                                                        <button class="btn btn-outline-primary btn-md" id="tanda_tangan" data-transaksi="" data-bs-toggle="modal" data-bs-target="#modalHalamanTandaTangan" onclick="halamanTandaTangan(event.target)">Tanda Tangan</button>
+                                                    </a>
                                                 </div>
                                             </div>
                                         </li>
@@ -64,7 +66,9 @@
                                                 <div class="check_administrator mb-3">
                                                 </div>
                                                 <div>
-                                                    <a href="#" class="btn btn-primary btn-sm">Tanda Tangan</a>
+                                                    <a href="#">
+                                                        <button class="btn btn-outline-primary btn-md" id="tanda_tangan" data-transaksi="" data-bs-toggle="modal" data-bs-target="#modalHalamanTandaTangan" onclick="halamanTandaTangan(event.target)">Tanda Tangan</button>
+                                                    </a>
                                                 </div>
                                             </div>
                                         </li>
@@ -75,7 +79,9 @@
                                                 <div class="check_penerima mb-3">
                                                 </div>
                                                 <div>
-                                                    <a href="#" class="btn btn-primary btn-sm">Tanda Tangan</a>
+                                                    <a href="#">
+                                                        <button class="btn btn-outline-primary btn-md" id="tanda_tangan" data-transaksi="" data-bs-toggle="modal" data-bs-target="#modalHalamanTandaTangan" onclick="halamanTandaTangan(event.target)">Tanda Tangan</button>
+                                                    </a>
                                                 </div>
                                             </div>
                                         </li>
@@ -86,7 +92,9 @@
                                                 <div class="check_penyerah mb-3">
                                                 </div>
                                                 <div>
-                                                    <a href="#" class="btn btn-primary btn-sm">Tanda Tangan</a>
+                                                    <a href="#">
+                                                        <button class="btn btn-outline-primary btn-md" id="tanda_tangan" data-transaksi="" data-bs-toggle="modal" data-bs-target="#modalHalamanTandaTangan" onclick="halamanTandaTangan(event.target)">Tanda Tangan</button>
+                                                    </a>
                                                 </div>
                                             </div>
                                         </li>
@@ -98,15 +106,36 @@
                 </div> 
             </div>
             <div class="modal-footer"> 
-                <button type="button" class="update-status btn btn-outline-danger" data-transaksi="" data-notransaksi="" data-bs-dismiss="modal"> Kembali </button>
+                <button type="button" class="btn btn-outline-danger" data-transaksi="" data-notransaksi="" data-bs-dismiss="modal"> Kembali </button>
                 <a href="" id="unduhDokumen" target=”_blank”>
-                    <button type="button" class="update-status btn btn-outline-primary"> Lihat Dokumen </button>
+                    <button type="button" class="btn btn-outline-primary"> Lihat Dokumen </button>
                 </a>
             </div>
         </div>
     </div>
 </div>
-<!--Modal show laporan-pengajuan-->\
+<!--Modal show tanda tangan-->
+
+<!--Modal show halaman tanda tangan-->
+<div class="modal fade" id="modalHalamanTandaTangan" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="laporan-pengajuan-title">Link Halaman Tanda Tangan</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="url_halaman_tanda_tangan" style="text-align: center;" data-id="">
+                    {!! QrCode::style('dot')->eye('circle')->size(200)->generate('https://www.facebook.com/'); !!}
+                </div>
+            </div>
+            <div class="modal-footer"> 
+                <button type="button" class="update-status btn btn-outline-danger" data-transaksi="" data-notransaksi="" data-bs-toggle="modal" data-bs-target="#modalAksi" onclick="lihatDokumen(event.target)" > Kembali </button>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Modal show halaman tanda tangan-->
     
 <script>
     $.ajaxSetup({
@@ -165,10 +194,10 @@
         var newURL = URL.replace(':id', id);
         $('#unduhDokumen').attr("href", newURL);
         $('#unduhDokumen').attr("data-id", id);
+        $('#tanda_tangan').attr("data-transaksi", id);
 
         var urlGetDokumen = "{{route('get-dokumen',':id')}}";
         var newUrlGetDokumen= urlGetDokumen.replace(':id', id)
-        // alert(newUrlGetDokumen)
         $.ajax({
             url: newUrlGetDokumen,
             method:"GET",
@@ -217,15 +246,16 @@
                        ` <i class="event-date text-success bx bx-md bx-check-circle"></i>`
                     )
                 }
-                // <i class="bx bx-md bx-x-circle" style="color: #ff3e1d;"></i>
-                console.log(dokumenPenyerahan.kasubumum_user.name)
-                console.log(dokumenPenyerahan.administrator_user.jabatan.jabatan)
-                console.log(dokumenPenyerahan.penerima_user.name)
-                // if(dokumenPenyerahan.kasubagUmumUser=="0"){
-                //     alert('kosong')
-                // }
             }
         })
+
+    }
+
+    function halamanTandaTangan(event){
+        var id = $(event).attr('data-transaksi');
+        var urlQRCode = "{{route('get-dokumen',':id')}}";
+        var newurlQRCode= urlQRCode.replace(':id', id)
+        $('.url_halaman_tanda_tangan').attr('data-id',id)
 
     }
 
@@ -288,53 +318,6 @@
         });
     };
 
-    function detailLaporanPengajuanTidakTersedia (id){
-        var URL = "{{route('laporan-barang-tidak-tersedia.show', ':id')}}";
-        var newURL = URL.replace(':id', id);
-        $('#detail-laporan-tidak-tersedia-datatable').DataTable({
-            processing: true,
-            serverSide: true, 
-            responsive: true,
-            searching :false,
-            bDestroy: true,
-            ajax: {
-                url:newURL,
-                type: 'GET'
-            }, columns: [
-                { "data": null,"sortable": false, 
-                    render: function (data, type, row, meta) {
-                            return meta.row + meta.settings._iDisplayStart + 1;
-                            }  
-                },{
-                    data: 'nama_barang',
-                    name: 'nama_barang'
-                },
-                {
-                    data: 'jumlah_barang',
-                    name: 'jumlah_barang'
-                },
-                {
-                    data: 'satuan',
-                    name: 'satuan'
-                },
-                {
-                    data: 'status',
-                    name: 'status'
-                },
-                {
-                    data: 'laporan_pengajuan',
-                    name: 'laporan_pengajuan'
-                },
-                {
-                    data: 'jumlah_disesuaikan',
-                    name: 'jumlah_disesuaikan'
-                },
-            ],
-                order: [
-                    [0, 'desc']
-            ]
-        });
-    };
     
 </script>
 @endsection
